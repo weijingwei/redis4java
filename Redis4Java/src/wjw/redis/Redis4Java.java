0e716c7d113edacc618a90b2016db01eb13b7f84
weijingwei <weijingwei@sina.com>
2016-09-07 16:26:19 +0800

    Redis 4 java

---
+++ Redis4Java/src/wjw/redis/Redis4Java.java
@@ -0,0 +1,20 @@
+package wjw.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class Redis4Java {
+	public static void main(String[] args) {
+		 //连接本地的 Redis 服务
+	      Jedis jedis = new Jedis("localhost");
+	      System.out.println("Connection to server sucessfully");
+	      // AUTH "passw0rd"
+	      jedis.auth("passw0rd");
+	      //查看服务是否运行
+	      System.out.println("Server is running: "+jedis.ping());
+	      
+	      jedis.set("javakey", "hello redis 4 java");
+	      System.out.println(jedis.get("javakey"));
+	      System.out.println(jedis.keys("*"));
+	      System.out.println(jedis.lrange("mylist", 0, 10));
+	}
+}
